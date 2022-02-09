c66e6e47cc3afbb4166dde0040cf7fc92041c6d4
runhui2010 <fredhuang128@yahoo.com>
2022-02-08 18:19:36 -0800

    Revert "update url" /bin/bash: qqq: command not found This reverts commit b2dbce065b4676870a21824eef9d8c82f9b542f5.

--- components/Img.js
+++ components/Img.js
@@ -1,10 +1,9 @@
 import Image from "next/image";
 
 export default function Img({ name }) {
-  const path='/../public/'+name
   return (
     <div className='relative'>
-      <Image src={path} width={40} height={60} objectFit="contain" alt="cover" />
+      <Image src={name} width={40} height={60} objectFit="contain" alt="cover" />
     </div>
   );
 }
