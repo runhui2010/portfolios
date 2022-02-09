1060b8b9859c373841d8370ae455e40a4a28a28a
runhui2010 <fredhuang128@yahoo.com>
2022-02-08 16:50:45 -0800

    typo

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Head from "next/Head";
+import Head from "next/head";
 import Link from "next/link";
 import { GoHome } from "react-icons/go";
 import { SiIota } from "react-icons/si";
