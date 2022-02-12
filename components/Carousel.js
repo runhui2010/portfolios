9481b1ead2282f2faa453855d3e77f1712dba3af
runhui2010 <fredhuang128@yahoo.com>
2022-02-12 00:27:07 -0800

    change fec to iframe

--- components/Carousel.js
+++ components/Carousel.js
@@ -3,20 +3,29 @@ import {
   BsFillCaretLeftSquareFill,
   BsFillCaretRightSquareFill,
 } from "react-icons/bs";
-
+import ReactPlayer from "react-player";
 
 export default function Carsousel({ index, setIndex, path }) {
+  const URL="https://www.youtube.com/embed/AljapeOjU3M";
   return (
-    <div className="relative w-full h-full">
-      <BsFillCaretLeftSquareFill
-        className="absolute inset-y-2/4 -left-8 z-50 text-4xl rounded-full hover:bg-red-500"
-        onClick={() => setIndex(index === 0 ? path.length - 1 : index - 1)}
-      />
-      <Image src={path[index]} layout="fill" objectFit="contain" alt="cover" />
+    // <div className="relative w-full h-full">
+    //   <BsFillCaretLeftSquareFill
+    //     className="absolute inset-y-2/4 -left-8 z-50 text-4xl rounded-full hover:bg-red-500"
+    //     onClick={() => setIndex(index === 0 ? path.length - 1 : index - 1)}
+    //   />
+    //   <Image src={path[index]} layout="fill" objectFit="contain" alt="cover" />
 
-      <BsFillCaretRightSquareFill
-        className="absolute inset-y-2/4 -right-8 z-50 text-4xl rounded-full hover:bg-red-500"
-        onClick={() => setIndex(index == path.length - 1 ? 0 : index + 1)}
+    //   <BsFillCaretRightSquareFill
+    //     className="absolute inset-y-2/4 -right-8 z-50 text-4xl rounded-full hover:bg-red-500"
+    //     onClick={() => setIndex(index == path.length - 1 ? 0 : index + 1)}
+    //   />
+    // </div>
+    <div className=" w-full h-full">
+      <ReactPlayer
+        url={URL}
+        playing={true}
+        width='100%'
+        height='100%'
       />
     </div>
   );
